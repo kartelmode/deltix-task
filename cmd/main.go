3bb6e7b7575ac661527246bc4360b4829af08615
kartelmode <losevgg@gmail.com>
2024-06-07 21:41:34 +0300

    added brute

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"os"
 
-	"github.com/kartelmode/deltix-task/internal/combiner"
 	"github.com/kartelmode/deltix-task/internal/configs"
-	"github.com/kartelmode/deltix-task/internal/converter"
-	"github.com/kartelmode/deltix-task/internal/manager"
 	"github.com/kartelmode/deltix-task/internal/serializer"
 	"github.com/kartelmode/deltix-task/internal/writer"
 )
+
+var currencies map[string]float64 = make(map[string]float64)
+var users map[string]float64 = make(map[string]float64)
+var usersTransactions map[string][]float64 = make(map[string][]float64)
+var usersTransactionsTime map[string][]int64 = make(map[string][]int64)
+
+func GetPrice(currency string, delta float64) float64 {
+	if currency == "USD" {
+		return delta
+	}
+	return currencies[currency+"USD"]
+}
 
 func main() {
 	if len(os.Args) > 2 {
@@ -23,12 +32,60 @@ func main() {
 	userData := serializer.UnmarshalUserData()
 	market := serializer.UnmarshalMarket()
 
-	pricing := converter.MakePricing(market)
-	manager := manager.MakeManager(pricing, userData)
-	combiner := combiner.MakeCombiner(userData[0].Timestamp)
+	for _, element := range userData {
+		_, ok := users[element.UserId]
+		if !ok {
+			users[element.UserId] = 0
+		}
+	}
 	writer := writer.MakeWriter()
 	writer.CreateCsvIfNotExist(options.AnswerFilename)
-	manager.Run(options.DeltaTimestamp, options.TimestampsPerGoroutine, options.GoroutinesCount, combiner)
-	combiner.Combine(writer, int64(options.DeltaTimestamp))
-	writer.CloseFile()
+
+	leftTime := userData[0].Timestamp
+	pointerUser := 0
+	pointerCurrencies := 0
+	for leftTime < userData[len(userData)-1].Timestamp {
+		for pointerUser < len(userData) && userData[pointerUser].Timestamp < leftTime+int64(options.DeltaTimestamp) {
+			for pointerCurrencies < len(market) && market[pointerCurrencies].Timestamp <= userData[pointerUser].Timestamp {
+				currencies[market[pointerCurrencies].Currency] = market[pointerCurrencies].Price
+				pointerCurrencies++
+			}
+			id := userData[pointerUser].UserId
+			usersTransactions[id] = append(usersTransactions[id], GetPrice(userData[pointerUser].Currency, userData[pointerUser].Delta))
+			usersTransactionsTime[id] = append(usersTransactionsTime[id], userData[pointerUser].Timestamp)
+			pointerUser++
+		}
+		for user := range users {
+			minv := users[user]
+			maxv := users[user]
+			sum := float64(0)
+			p := 0
+			last := leftTime
+			for _, transaction := range usersTransactions[user] {
+				sum += users[user] * float64(usersTransactionsTime[user][p]-last)
+				users[user] += transaction
+				minv = min(minv, users[user])
+				maxv = max(maxv, users[user])
+				last = usersTransactionsTime[user][p]
+				p++
+			}
+			sum += users[user] * float64(leftTime+int64(options.DeltaTimestamp)-last)
+			sum /= float64(options.DeltaTimestamp)
+			writer.Write(user, minv, maxv, sum, leftTime)
+			usersTransactions[user] = make([]float64, 0)
+			usersTransactionsTime[user] = make([]int64, 0)
+		}
+		leftTime += int64(options.DeltaTimestamp)
+	}
+
+	return
+
+	// pricing := converter.MakePricing(market)
+	// manager := manager.MakeManager(pricing, userData)
+	// combiner := combiner.MakeCombiner(userData[0].Timestamp)
+	// writer := writer.MakeWriter()
+	// writer.CreateCsvIfNotExist(options.AnswerFilename)
+	// manager.Run(options.DeltaTimestamp, options.TimestampsPerGoroutine, options.GoroutinesCount, combiner)
+	// combiner.Combine(writer, int64(options.DeltaTimestamp))
+	// writer.CloseFile()
 }
